f3fe31636b1d4865cd44bed12ade31e0fe26b6b2
Ranjith Suranga <d.m.ranjith.sura@gmail.com>
2024-06-12 02:51:28 +0530

    Finalize with the dark mode and light mode

--- projects/to-do-list/js/index.js
+++ projects/to-do-list/js/index.js
@@ -10,6 +10,17 @@ const listContainerElm = document
 const noItemsElm = document
     .querySelector("#list-container > div");
 
+const modeElm = document
+    .querySelector("#mode");
+
+modeElm.addEventListener('change', ()=>{
+   if (modeElm.checked){
+       document.querySelector('html').setAttribute('data-bs-theme', 'dark');
+   }else{
+       document.querySelector('html').removeAttribute('data-bs-theme');
+   }
+});
+
 btnAddTaskElm.addEventListener('click', (e)=>{
     e.preventDefault();
     const task = txtTaskElm.value.trim();
@@ -40,10 +51,12 @@ btnAddTaskElm.addEventListener('click', (e)=>{
     listItemElm.append(iconElm);
 
     listItemElm.setAttribute("class", "list-item d-flex justify-content-between p-2 align-items-center animate__animated animate__fadeInUp");
+    listItemElm.setAttribute("tabindex", "0");
     lblElm.setAttribute("class", "d-flex gap-2");
     inputElm.setAttribute("type", "checkbox");
-    inputElm.setAttribute("class", "form-check");
-    iconElm.setAttribute("class", "bi bi-trash3");
+    inputElm.setAttribute("tabindex", "-1");
+    inputElm.setAttribute("class", "form-check-input");
+    iconElm.setAttribute("class", "bi bi-trash3 me-2");
 
     listContainerElm.append(listItemElm);
     txtTaskElm.value = "";
@@ -53,10 +66,32 @@ btnAddTaskElm.addEventListener('click', (e)=>{
 listContainerElm.addEventListener('click',
     (e)=>{
     if (e.target.getAttribute("class") ===
-        'bi bi-trash3'){
+        'bi bi-trash3 me-2'){
         e.target.parentElement.remove();
         if (!listContainerElm.children.length){
             listContainerElm.append(noItemsElm);
         }
+    }
+});
+
+listContainerElm.addEventListener('keydown', (e)=>{
+    switch (e.code){
+        case "ArrowUp":
+            e.target.closest(".list-item").previousElementSibling?.focus();
+            break;
+        case "ArrowDown":
+            e.target.closest(".list-item").nextElementSibling?.focus();
+            break;
+        case "Space":
+            const checkboxElm = e.target.closest(".list-item").querySelector("input");
+            checkboxElm.checked = !checkboxElm.checked;
+            break;
+        case "Delete":
+            let previousElm = e.target.closest(".list-item").previousElementSibling;
+            const iconElm = e.target.closest(".list-item")
+                .querySelector("i");
+            iconElm.click();
+            previousElm = previousElm ?? document.querySelector(".list-item");
+            previousElm?.focus();
     }
 });
